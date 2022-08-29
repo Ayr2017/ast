607ec9f49c35402e2d7d8a53fa46261db8cefb8e
Ayrat <ayrat.2013@yandex.ru>
2022-08-29 21:09:38 +0300

    FIX prev

--- app/Actions/Admin/Users/GetUsers.php
+++ app/Actions/Admin/Users/GetUsers.php
@@ -11,26 +11,26 @@ class GetUsers
     public function execute($request)
     {
         if($request->select == 'withoutTrashed' ) {
-            $users = User::with('roles')->get();
+            $users = User::with('roles');
         } elseif($request->select == 'trashed' ){
-            $users = User::onlyTrashed()->with('roles')->get();
+            $users = User::onlyTrashed()->with('roles');
         } else {
-            $users = User::withTrashed()->with('roles')->get();
+            $users = User::withTrashed()->with('roles');
         }
 
         return $this->filterUsers($users);
     }
 
-    private function filterUsers(Collection $users)
+    private function filterUsers( $users)
     {
         $user = auth()->user();
 
         if($user->hasRole('super-admin')){
-            return $users->where('id', '!=', $user->id);
+            return $users->where('id', '!=', $user->id)->get();
         } elseif($user->hasRole('admin')){
 //            TODO: доделать это место. Выводить только тех кто не админ и суперадмин.
-            $newUsers =  User::doesntHave('roles')->get();
-            return  User::role('specialist')->get()->merge($newUsers);
+            $newUsers =  $users->doesntHave('roles')->get();
+            return  $users->role('specialist')->get()->merge($newUsers);
         }
     }
 }
