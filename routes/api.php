cf552fe4628e0c092c8a337fec0099ca2cd2e639
Ayrat <ayrat.2013@yandex.ru>
2023-02-10 20:59:19 +0300

    Добавлены новые маршруты и контроллеры к апи

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\v1\ContactsController;
 use App\Http\Controllers\Api\v1\DistrictsController;
+use App\Http\Controllers\Api\v1\FarmsController;
+use App\Http\Controllers\Api\v1\FieldTemplatesController;
+use App\Http\Controllers\Api\v1\FormCategoriesController;
+use App\Http\Controllers\Api\v1\FormFieldsController;
+use App\Http\Controllers\Api\v1\FormsController;
+use App\Http\Controllers\Api\v1\OrganisationsController;
 use App\Http\Controllers\Api\v1\RegionsController;
+use App\Http\Controllers\Api\v1\ReportsController;
+use App\Http\Controllers\Api\v1\RolesController;
+use App\Http\Controllers\Api\v1\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +31,16 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('v1')->middleware('auth:api')->name('v1')->group(function(){
-    Route::resource('regions', RegionsController::class);
+    Route::resource('contacts', ContactsController::class);
     Route::resource('districts', DistrictsController::class);
+    Route::resource('farms', FarmsController::class);
+    Route::resource('field-templates', FieldTemplatesController::class);
+    Route::resource('form-categories', FormCategoriesController::class);
+    Route::resource('form-fields', FormFieldsController::class);
+    Route::resource('forms', FormsController::class);
+    Route::resource('organisations', OrganisationsController::class);
+    Route::resource('regions', RegionsController::class);
+    Route::resource('reports', ReportsController::class);
+    Route::resource('roles', RolesController::class);
+    Route::resource('users', UsersController::class)->middleware('role:super-admin|admin');
 });
