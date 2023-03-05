6c0e34c36a80cff6691d1b163e4d7a8f930c72b3
Ayrat <ayrat.2013@yandex.ru>
2023-03-05 23:04:44 +0300

    Исправлени UpdateFormField

--- app/Actions/Admin/FormFields/UpdateFormField.php
+++ app/Actions/Admin/FormFields/UpdateFormField.php
@@ -9,6 +9,10 @@ class UpdateFormField
     public function execute($validatedRequest, $id)
     {
         $formField = FormField::withTrashed()->find($id);
+        $type = $validatedRequest->type;
+        if($type != 'select' || $type != 'checkbox'|| $type != 'radio') {
+            $validatedRequest['select_fields'] = null;
+        }
         $formField->update($validatedRequest);
         return $formField;
     }
