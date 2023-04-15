4b8f38b182681e11a9ac4ec51c5948007615658e
Ayrat <ayrat.2013@yandex.ru>
2023-04-15 14:44:58 +0300

    Добавление forceSchema('https')

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\Interfaces\Contactable;
 use App\Models\Organization;
 use App\Models\User;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
@@ -26,14 +27,18 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
         Paginator::useBootstrap();
-        DB::listen(function ($query) {
+        if(env('APP_ENV') !== 'production')
+        {
+            $url->forceSchema('https');
+        }
+//        DB::listen(function ($query) {
 //            $query->sql; // выполненная sql-строка
 //            $query->bindings; // параметры, переданные в запрос (то, что подменяет '?' в sql-строке)
 //            $query->time; // время выполнения запроса
 //            Log::info([$query->sql, $query->time ]);
-        });
+//        });
     }
 }
