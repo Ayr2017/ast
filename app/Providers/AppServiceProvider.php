6275c29c33e755b7e0156765f00317112b916d5e
Ayrat <ayrat.2013@yandex.ru>
2023-04-15 15:30:25 +0300

    Добавление forceSchema('https')++

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,7 +31,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        if(env('APP_ENV') !== 'production')
+        if(env('APP_ENV') == 'production')
         {
             URL::forceScheme('https');
         }
