5febcaaa4e7c7816e19c349eaa3cec1740fafdb9
userixxx <131803136+userixxx@users.noreply.github.com>
2023-06-18 17:52:36 +0300

    Add files via upload

--- app/Http/Controllers/Api/v1/PasswordResetsController.php
+++ app/Http/Controllers/Api/v1/PasswordResetsController.php
@@ -3,81 +3,32 @@
 namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
-use App\Models\Organization;
-use App\Models\PasswordReset;
+use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 use Illuminate\Http\Request;
-use Carbon\Carbon;
-use Illuminate\Support\Str;
-
+use Illuminate\Support\Facades\Password;
 
 class PasswordResetsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        $passwordResets = PasswordReset::all();
-        return response($passwordResets, 200);
-    }
+    use SendsPasswordResetEmails;
 
-    public function updateOrCreate(Request $request, $email)
+    /**
+     * Отправить письмо со ссылкой на сброс пароля.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendResetLinkEmail(Request $request)
     {
-        $passwordReset = PasswordReset::updateOrCreate(
-            ['email' => $email],
-            [
-                'token' => Str::uuid(),
-                'created_at' => Carbon::now()
-            ]
+        $this->validateEmail($request);
+
+        $response = $this->broker()->sendResetLink(
+            $this->credentials($request)
         );
 
-        return response(['passwordReset' => $passwordReset], 200);
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        if ($response == Password::RESET_LINK_SENT) {
+            return response()->json(['message' => 'Reset password email sent.']);
+        } else {
+            return response()->json(['message' => 'Unable to send reset password email.'], 500);
+        }
     }
 }
